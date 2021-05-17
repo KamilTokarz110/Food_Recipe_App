89dbab9c420d2f6f58f048a7ee778d28315a5cde
Kamil Tokarz <k.tokarz@praktyki.ideo.pl>
2021-05-17 18:35:24 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+const searchForm = document.querySelector('form');
+const searchResult = document.querySelector('.search-resoult');
+const sectionMain = document.querySelector('.main');
+let searchString = '';
+//API
+const APP_ID = 'cd752456';
+const APP_KEY = '5674e31331fc13520e5c996c79b2471a';
+
+
+searchForm.addEventListener('submit', (e)=>{
+e.preventDefault();
+searchString = e.target.querySelector('input').value;
+fetchAPI();
+
+});
+async function fetchAPI (){
+    const baseURL = `https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}&to=20`;
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    generateHTML(data.hits);
+    console.log(data);
+}
+function generateHTML(results){
+    let generatedHTML ='';
+    results.map(result => {
+        generatedHTML +=
+        `
+        <div class="search-resoult__item">
+        <img class="search-resoult__item-img" src="${result.recipe.image}" alt="">
+        <div class="search-resoult__item-info">
+            <h1 class="search-resoult__item-title">${result.recipe.label}</h1>
+            <a href="${result.recipe.url}" target="_blank" class="search-resoult__item-link">Link</a>
+        </div>
+        <p class="search-resoult__item-kcal">Calories: ${result.recipe.calories.toFixed(0)}</p>
+        <p class="search-resoult__item-kcal">Diet label: ${result.recipe.dietLabels.lenght > 0 ? result.recipe.dietLabels.lenght : 'No Data Found'}</p>
+        <p class="search-resoult__item-kcal">Health Label: ${result.recipe.healthLabels}</p>
+    </div>
+    `
+
+    })
+    searchResult.innerHTML = generatedHTML;
+}
